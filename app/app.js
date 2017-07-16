cc168665116e8f618d32350bb7cb2298e540ecc4
Ivan Hoo <ihoo@sfu.ca>
2017-07-15 23:11:20 -0700

    finish db setup

--- app/app.js
+++ app/app.js
@@ -5,11 +5,15 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var csrf = require('csurf');
 
+//controllers
 var index = require('./routes/index');
 var users = require('./routes/users');
 
-var db = require('./model/db');
+//models
+var db = require('./models/db');
+var user = require('./models/user');
 
 var app = express();
 
@@ -26,25 +30,32 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// csrf setup
+app.use(csrf({ cookie: true }));
+app.use(function(req, res, next) {
+	res.locals.csrfToken = req.csrfToken() ;
+	next();
+});
+
 app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
