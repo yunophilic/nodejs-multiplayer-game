0897b59ad38d5402a4a1359ca167667d9e5a4aa1
Ivan Hoo <ihoo@sfu.ca>
2017-07-16 23:23:39 -0700

    finish setup ui layout

--- app/app.js
+++ app/app.js
@@ -29,6 +29,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
+app.use('/jquery', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
 
 // csrf setup
 app.use(csrf({ cookie: true }));
