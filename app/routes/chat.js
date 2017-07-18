3ef1327550d93469db62e4594616202eb0c2d13e
jinquanj <jinquanj@sfu.ca>
2017-07-17 23:12:17 -0700

    set socket.io, and adding a submit button

--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+var http = require('http').Server(express);
+var io = require('socket.io')(http);
 /* GET chatting room page. */
 router.get('/', function(req, res, next) {
   res.render('chat/index', { title: 'Chatting' });
+});
+io.on('connection', function(socket){
+  console.log('a user connected');
 });
 
 module.exports = router;
