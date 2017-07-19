7d33a5695b64271bb3aa48506638818a99130620
jinquanj <jinquanj@sfu.ca>
2017-07-18 20:47:47 -0700

    merge from master for login changes

--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var http = require('http').Server(express);
-var io = require('socket.io')(http);
+// var io = require('socket.io')(http);
+var app = express();
+var server = app.listen(3000);
+var io = require('socket.io').listen(server);
 /* GET chatting room page. */
 router.get('/', function(req, res, next) {
   res.render('chat/index', { title: 'Chatting' });
