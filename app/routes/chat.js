1ea24fb414183be799b20e9200ecf18fbd88bb4c
jinquanj <jinquanj@sfu.ca>
2017-07-19 22:33:30 -0700

    finished real time chatting without css and db setting

--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,18 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var http = require('http').Server(express);
-// var io = require('socket.io')(http);
-var app = express();
-var server = app.listen(3000);
-var io = require('socket.io').listen(server);
+
 /* GET chatting room page. */
 router.get('/', function(req, res, next) {
   res.render('chat/index', { title: 'Chatting' });
 });
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
-});
-
 module.exports = router;
