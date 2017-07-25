5c9bdb595d2bcd44265e75a8f15f800695fa076d
jinquanj <jinquanj@sfu.ca>
2017-07-24 23:30:59 -0700

    add access control for chatting room

--- app/routes/chat.js
+++ app/routes/chat.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var middlewares = require('../utils/middlewares');
 var router = express.Router();
 
 /* GET chatting room page. */
-router.get('/', function(req, res, next) {
-  res.render('chat/index', { title: 'Chatting' });
+// router.get('/', function(req, res, next) {
+//   res.render('chat/index', { title: 'Chatting' });
+// });
+router.get('/', middlewares.isLoggedIn, function(req, res) {
+  res.render('chat/index', {
+    user : req.user // get the user out of session and pass to template
+  });
 });
 module.exports = router;
