2113e388d1d394f8104cdf23a91e4ba4fa6190ce
kawahc <kawahc@sfu.ca>
2017-07-26 02:09:19 -0700

    modify the menu bar

--- app/routes/friends.js
+++ app/routes/friends.js
@@ -39,7 +39,57 @@ router.get('/', function(req, res, next) {
 	});
 });
 
+// route middleware to validate :id
+router.param('id', function(req, res, next, id) {
+	//console.log('validating ' + id + ' exists');
+	//find the ID in the Database
+	mongoose.model('User').findById(id, function (err, user) {
+		//if it isn't found, we are going to respond with 404
+		if (err) {
+			console.log(id + ' was not found');
+			res.status(404)
+			var err = new Error('Not Found');
+			err.status = 404;
+			res.format({
+				html: function() {
+					next(err);
+				},
+				json: function() {
+					res.json({message : err.status	+ ' ' + err});
+				}
+			});
+		//if it is found we continue on
+		} else {
+			//uncomment this next line if you want to see every JSON document response for every GET/PUT/DELETE call
+			//console.log(user);
+			// once validation is done save the new item in the req
+			req.id = id;
+			// go to the next thing
+			next();
+		}
+	});
+});
 
-
+router.get('/:id', function(req, res) {
+	mongoose.model('User').findById(req.id, function (err, user) {
+		if (err) {
+			console.log('GET Error: There was a problem retrieving: ' + err);
+		} else {
+			console.log('GET Retrieving ID: ' + user._id);
+			//var userDob = user.dob.toISOString().substring(0, userdob.indexOf('T'));
+			res.format({
+        html: function(){
+					res.render('users/show', {
+						title: 'Users Profile',
+						"users" : users
+					});
+				},
+				json: function(){
+					res.json(user);
+				}
+			});
+		}
+	});
+});
 
 module.exports = router;
