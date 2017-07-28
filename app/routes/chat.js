152c4ed15137cfd254ce822074e6061a277b7d70
Ivan Hoo <ihoo@sfu.ca>
2017-07-28 01:44:41 -0700

    fix layout

--- app/routes/chat.js
+++ app/routes/chat.js
@@ -3,15 +3,15 @@ var middlewares = require('../utils/middlewares');
 var router = express.Router();
 
 /* GET chatting room page. */
-// router.get('/', function(req, res, next) {
-//   res.render('chat/index', { title: 'Chatting' });
-// });
 router.get('/', middlewares.isLoggedIn, function(req, res) {
-  res.render('chat/index', {
-    user : req.user // get the user out of session and pass to template
-  });
+	res.render('chat/index', {
+		user : req.user, // get the user out of session and pass to template
+		layout: 'layouts/no-container'
+	});
 });
+
 router.get('/chatname', middlewares.isLoggedIn, function(req, res, next) {
 	res.json({chatname: req.user.local.username});
 });
+
 module.exports = router;
